f360182951d2e5dfa30db17bc55e34ef3ba11c2b
Fuinked <gylxlishizhi@mail.ustc.edu.cn>
2024-04-05 19:42:50 +0800

    访问设备

--- 平台信息.cpp
+++ 平台信息.cpp
@@ -6,30 +6,119 @@
 #else
 #include<CL/cl.h>
 #endif
-using namespace::std;
+using namespace  std;
 int main(void)
 {
 	cl_platform_id* platform=0;
 	cl_uint num_platform;
 	cl_uint i;
 	cl_int err;
-	char* ext_data;
-	size_t ext_size;
-	err = clGetPlatformIDs(10, NULL, &num_platform);
+	ofstream fout;
+	fout.open("out.txt");
+	struct platinfo {
+		char* name;
+		char* vendor;
+		char* version;
+		char* profile;
+		char* extensions;
+	};
+	size_t size;
+	err = clGetPlatformIDs(0, NULL, &num_platform);
+	//第一次调用clGetPlatformIDs，获取总平台数，第一个参数无效
 	if (err < 0)
 	{
+		cout << "无法获取platformids";
 		exit(-1);
 	}
 	
 	try 
 	{
 		platform = new cl_platform_id[sizeof(cl_platform_id) * num_platform];
+		//根据平台数分配内存
 	}
 	catch (std::bad_alloc)
 	{
 		cout << "分配platform内存失败";
+		exit(-1);
 	}
 	clGetPlatformIDs(num_platform, platform, NULL);
+	//获取平台ID
+	struct platinfo info = { 0 };
+	for (i = 0; i < num_platform; i++)
+	{
+		clGetPlatformInfo(platform[i], CL_PLATFORM_NAME, 0, NULL, &size);
+		//获取平台名称的长度
+		try 
+		{
+			info.name = new char[sizeof(char) * size];
+		}
+		catch (std::bad_alloc)
+		{
+			cout << "info.name内存分配失败";
+			exit(-1);
+		}
+		clGetPlatformInfo(platform[i], CL_PLATFORM_NAME, size, info.name, NULL);
+		//获取平台名称
+
+		clGetPlatformInfo(platform[i], CL_PLATFORM_VENDOR, 0, NULL, &size);
+		//获取平台厂商名称的长度
+		try
+		{
+			info.vendor = new char[sizeof(char) * size];
+		}
+		catch (std::bad_alloc)
+		{
+			cout << "info.vendor内存分配失败";
+			exit(-1);
+		}
+		clGetPlatformInfo(platform[i], CL_PLATFORM_VENDOR, size, info.vendor, NULL);
+		//获取平台厂商名称
+
+		clGetPlatformInfo(platform[i], CL_PLATFORM_VERSION, 0, NULL, &size);
+		//获取平台支持的最高OPenCL版本的字长
+		try
+		{
+			info.version = new char[sizeof(char) * size];
+		}
+		catch (std::bad_alloc)
+		{
+			cout << "info.version内存分配失败";
+			exit(-1);
+		}
+		clGetPlatformInfo(platform[i], CL_PLATFORM_VERSION, size, info.version, NULL);
+		//获取平台支持的最高OPenCL版本
+
+		clGetPlatformInfo(platform[i], CL_PLATFORM_PROFILE, 0, NULL, &size);
+		//
+		try
+		{
+			info.profile = new char[sizeof(char) * size];
+		}
+		catch (std::bad_alloc)
+		{
+			cout << "info.profile内存分配失败";
+			exit(-1);
+		}
+		clGetPlatformInfo(platform[i], CL_PLATFORM_PROFILE, size, info.profile, NULL);
+
+		clGetPlatformInfo(platform[i], CL_PLATFORM_EXTENSIONS, 0, NULL, &size);
+		try
+		{
+			info.extensions = new char[sizeof(char) * size];
+		}
+		catch (std::bad_alloc)
+		{
+			cout << "info.extensions内存分配失败";
+			exit(-1);
+		}
+		clGetPlatformInfo(platform[i], CL_PLATFORM_EXTENSIONS, size, info.extensions, NULL);
+
+		fout << "平台名称：" << info.name<<endl;
+		fout << "平台厂商：" << info.vendor << endl;
+		fout << "最高OPenCL版本：" << info.version << endl;
+		fout << "支持的完整性：" << info.profile << endl;
+		fout << "支持的拓展名列表：" << info.extensions << endl << endl;
+	}
 
 	return 0;
 }
---
+++ 访问设备/访问安装设备.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include<fstream>
+
+#ifdef MAC
+#include<OpenCL/cl.h>
+#else
+#include<CL/cl.h>
+#endif
+using namespace std;
+int main(void)
+{
+	cl_platform_id* platform = 0;
+	cl_uint num_platform,num_devices;
+	cl_uint i;
+	cl_int err;
+
+	cl_device_id* devices;
+
+
+	ofstream fout;
+	fout.open("out.txt");
+	err = clGetPlatformIDs(10, NULL, &num_platform);
+	if (err < 0)
+	{
+		cout << "无法获取platformids";
+		exit(-1);
+	}
+
+	try
+	{
+		platform = new cl_platform_id[sizeof(cl_platform_id) * num_platform];
+	}
+	catch (std::bad_alloc)
+	{
+		cout << "分配platform内存失败";
+		exit(-1);
+	}
+	clGetPlatformIDs(num_platform, platform, NULL);
+
+
+
+	for (i = 0; i < num_platform; i++)
+	{
+		err = clGetDeviceIDs(platform[i], CL_DEVICE_TYPE_ALL, 1, NULL, &num_devices);
+		if (err < 0)
+		{
+			cout << "无法获取第" << i+1 << "个平台的设备信息。";
+			exit(-1);
+		}
+		else
+		{
+			try
+			{
+				devices = new cl_device_id[sizeof(cl_device_id) * num_devices];
+			}
+			catch (std::bad_alloc)
+			{
+				cout << "分配devices内存失败";
+				exit(-1);
+			}
+			clGetDeviceIDs(platform[i], CL_DEVICE_TYPE_ALL, num_devices, devices, NULL);
+			fout << "平台编号：" << i << endl;
+			for (int j = 0; j < num_devices; j++)
+			{
+				size_t size;
+				char* devices_name;
+				clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 0, NULL, &size);
+				devices_name = new char[sizeof(char) * size];
+				clGetDeviceInfo(devices[j], CL_DEVICE_NAME, size, devices_name, NULL);
+				fout << "设备名称：" << devices_name << endl;
+
+				clGetDeviceInfo(devices[j], CL_DEVICE_VENDOR, 0, NULL, &size);
+				devices_name = new char[sizeof(char) * size];
+				clGetDeviceInfo(devices[j], CL_DEVICE_VENDOR, size, devices_name, NULL);
+				fout << "设备厂商：" << devices_name << endl;
+
+				clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, 0, NULL, &size);
+				devices_name = new char[sizeof(char) * size];
+				clGetDeviceInfo(devices[j], CL_DEVICE_EXTENSIONS, size, devices_name, NULL);
+
+				fout << "设备拓展：" << devices_name << endl;
+
+				cl_ulong mem_size;
+				clGetDeviceInfo(devices[j], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(mem_size), &mem_size, NULL);
+				fout << "设备全局内存大小：" << mem_size << endl;
+
+				clGetDeviceInfo(devices[j], CL_DEVICE_ADDRESS_BITS, sizeof(mem_size), &mem_size, NULL);
+				fout << "设备地址空间大小：" << mem_size << endl;
+
+				cl_bool x;
+				clGetDeviceInfo(devices[j], CL_DEVICE_AVAILABLE, sizeof(x), &x, NULL);
+				fout << "设备可用性：" << x << endl;
+
+				clGetDeviceInfo(devices[j], CL_DEVICE_COMPILER_AVAILABLE, sizeof(x), &x, NULL);
+				fout << "设备是否提供编译器：" << x << endl;
+			}
+			fout << endl;
+		}
+	}
+	return 0;
+}
